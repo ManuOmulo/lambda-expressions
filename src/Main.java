064e572b82130da6496a6f5c61e31013609cdf26
ManuOmulo <omuloemmanuel@gmail.com>
2024-02-22 23:56:57 +0300

    Added a Supplier functional interface

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public class Main {
   public static void main(String[] args) {
@@ -21,8 +23,18 @@ public class Main {
 
     coords.forEach(arr -> processPoint(arr[0], arr[1], (lat, lon) -> System.out.printf("lat: %.3f, long: %.3f\n", lat, lon)));
 
+    System.out.println("--------------------");
     list.removeIf(s -> s.equalsIgnoreCase("bravo"));
     list.forEach((el) -> System.out.println(el));
+
+    System.out.println("--------------------");
+    list.replaceAll((s) -> s.charAt(0) + " - " + s.toUpperCase());
+    list.forEach(el -> System.out.println(el));
+
+    System.out.println("--------------------");
+    String[] names = {"Anne", "Bob", "Caroline", "David", "Edward", "Fred"};
+    String[] randomList = randomSelectedValues(12, names, () -> new Random().nextInt(0, names.length));
+    System.out.println(Arrays.toString(randomList));
   }
 
   public static <T> T calculator(Operation<T> myFunction, T value1, T value2) {
@@ -33,5 +45,15 @@ public class Main {
 
   public static <T> void processPoint(T p1, T p2, BiConsumer<T, T> consumer) {
     consumer.accept(p1, p2);
+  }
+
+  public static String[] randomSelectedValues(int count, String[] values, Supplier<Integer> s) {
+    String[] selectedValues = new String[count];
+
+    for (int i = 0; i < count; i++) {
+      selectedValues[i] = values[s.get()];
+    }
+
+    return selectedValues;
   }
 }
